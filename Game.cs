e0fc77bbd0bd4a74e50a06cf87a6f1a773e588db
lodisperkins <lodisperkins44@gmail.com>
2020-09-23 14:37:05 -0500

    Initial commit

---
+++ Game.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Game
+    {
+        //Run the game
+        public void Run()
+        {
+            
+        }
+
+        //Performed once when the game begins
+        public void Start()
+        {
+            
+        }
+
+        //Repeated until the game ends
+        public void Update()
+        {
+            
+        }
+
+        //Performed once when the game ends
+        public void End()
+        {
+            
+        }
+    }
+}
